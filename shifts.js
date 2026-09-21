c246531fac8a1a0cd652972627c05875a3cb8143
Justine Bungabong <justinebungabon@gmail.com>
2026-09-21 18:07:47 +0800

    Add overtime pay for shifts over 8 hours - bungabong.justine

--- shifts.js
+++ shifts.js
@@ -3,7 +3,14 @@ function isValidShift(hours) {
 }
 
 function calculatePay(hours, rate) {
-  return Math.floor(hours * rate);
+  if(hours > 8){
+    let regularPay = 8 * rate;
+    let overtimeHours = hours - 8;
+    let overtimePay = overtimeHours * (rate * 1.5);
+    return regularPay + overtimePay;
+  }else{
+    return Math.floor(hours * rate);
+  }
 }
 
 module.exports = { isValidShift, calculatePay };
